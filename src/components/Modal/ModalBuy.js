971ce5c9c075ee69d6e2492a3a69e88a612b79a0
VenomSRT <ferrarif40lemans@gmail.com>
2021-01-13 16:33:41 +0200

    inputs js validation

--- src/components/Modal/ModalBuy.js
+++ src/components/Modal/ModalBuy.js
@@ -1,19 +1,85 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { toggleBuyModal, toggleSuccess } from "../../store/actions";
 import "./Modal.css";
 
 export const ModalBuy = () => {
   const dispatch = useDispatch();
+  const inputsValidStatus = {
+      name: false,
+      surname: false,
+      passport: false,
+      email: false,
+      phone: false
+  };
 
+  useEffect(() => {
+    const inputs = document.querySelectorAll('.modal__input');
+    inputs.forEach(input => {
+      input.addEventListener('blur', validator);
+      input.addEventListener('change', removeInvalid);
+    })
+  }, [])
+  
   function handleModal() {
     dispatch(toggleBuyModal());
   }
 
   function handleSuccsess(e) {
-    console.log(e);
     e.preventDefault();
-    dispatch(toggleSuccess());
+
+    if(Object.values(inputsValidStatus).some(value => !value)){
+      console.log('some input invalid');
+    } else {
+      dispatch(toggleSuccess());
+    }
+  }  
+
+  function validator(e) {
+    const nameRegExp = /[А-Я]{1}[а-я]{1,}/;
+    const passportRegExp = /[А-Я]{2}\d{6}/;
+    const emailRegExp = /[0-9a-z_A-Z]+@[0-9a-z_A-Z^.]+\.[a-zA-Z]{2,4}/;
+    const phoneRegExp = /[0-9]{10,}/;
+
+    const currentInput = e.target;
+    const inputName = currentInput.id;
+    const inputValue = currentInput.value;
+
+    let currentRegExp = null;
+
+    switch (inputName) {
+      case 'name':
+      case 'surname':
+        currentRegExp = nameRegExp;
+        break;
+
+      case 'passport':
+        currentRegExp = passportRegExp;
+        break;
+
+      case 'email':
+        currentRegExp = emailRegExp;
+        break;
+
+      case 'phone':
+        currentRegExp = phoneRegExp;
+        break;
+
+      default:
+        return;
+    }
+  
+    if (!currentRegExp.test(String(inputValue))) {
+      currentInput.classList.add('invalid');
+      inputsValidStatus[inputName] = false;
+    } else {
+      currentInput.classList.add('valid');
+      inputsValidStatus[inputName] = true;
+    }
+  }
+
+  function removeInvalid(e) {
+    e.target.classList.remove('invalid');
   }
 
   return (
@@ -31,7 +97,6 @@ export const ModalBuy = () => {
             className="modal__input form-control rounded"
             type="text"
             id="name"
-            pattern="[А-Я]{1}[а-я]{1,}"
             placeholder="Имя с большой буквы"
             required
           ></input>
@@ -44,7 +109,6 @@ export const ModalBuy = () => {
             className="modal__input form-control rounded"
             type="text"
             id="surname"
-            pattern="[А-Я]{1}[а-я]{1,}"
             placeholder="Фамилия с большой буквы"
             required
           ></input>
@@ -57,7 +121,6 @@ export const ModalBuy = () => {
             className="modal__input form-control rounded"
             type="text"
             id="passport"
-            pattern="[А-Я]{2}\d{6}"
             placeholder="АА123456"
             required
           ></input>
@@ -70,20 +133,18 @@ export const ModalBuy = () => {
             className="modal__input form-control rounded"
             type="email"
             id="email"
-            pattern="[0-9a-z_A-Z]+@[0-9a-z_A-Z^.]+\.[a-zA-Z]{2,4}"
             placeholder="example@mail.com"
             required
           ></input>
         </label>
 
-        <label htmlFor="telephone" className="modal__label form-label pt-3">
+        <label htmlFor="phone" className="modal__label form-label pt-3">
           <h3 className="modal__input-title fs-6">Телефон</h3>
 
           <input
             className="modal__input form-control rounded"
             type="text"
-            id="telephone"
-            pattern="[0-9]{10,}"
+            id="phone"
             placeholder="Минимум 10 цифр"
             required
           ></input>
